9c7538a9760db80cc98d3d1513662e493aeb731d
Leonel Rasjido <leonelivanrasjido@gmail.com>
2026-03-19 10:02:05 -0300

    Fix: Add estetica.synory.tech to CORS allowedOrigins

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,8 @@ const app = express();
 const allowedOrigins = [
     'https://synory.tech',
     'https://turnero.synory.tech',
+    'https://estetica.synory.tech',
+    process.env.FRONTEND_URL,
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
